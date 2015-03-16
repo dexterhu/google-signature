fa6be7c52f1bdc5a0db9dd9b756e63166becb869
Dexter Hu <hyhu84@gmail.com>
2015-03-16 15:24:14 -0700

    initial commit

--- test/main.js
+++ test/main.js
@@ -1,4 +1,3 @@
-
 "use strict";
 
 var should = require('should');
@@ -8,26 +7,29 @@ var signature = require('../lib/main');
 
 describe('signature.generate()', function() {
 
-    //
-    // signature.generate() - Without Any Argument
-    //
-    it('without any argument', function(done) {
-           signature.generate('','',function(signature){
-                signature.should.eql({ signature : '-9sdGxiqbAgyS31ktx-3Y3BpDh0' });
-                done();
-            });
+  //
+  // signature.generate() - Without Any Argument
+  //
+  it('without any argument', function(done) {
+    signature.generate('', '', function(signature) {
+      signature.should.eql({
+        signature: '-9sdGxiqbAgyS31ktx-3Y3BpDh0'
+      });
+      done();
     });
+  });
 
-    //
-    // signature.generate() - Without test test
-    //
-    it('with test test', function(done) {
-        signature.generate('test','test',function(signature){
-            signature.should.eql({ signature : 'me50L1BneaefWlbvb_XpsaIclo0' });
-            done();
-        });
+  //
+  // signature.generate() - Without test test
+  //
+  it('with test test', function(done) {
+    signature.generate('test', 'test', function(signature) {
+      signature.should.eql({
+        signature: 'me50L1BneaefWlbvb_XpsaIclo0'
+      });
+      done();
     });
+  });
 
 
 });
-
